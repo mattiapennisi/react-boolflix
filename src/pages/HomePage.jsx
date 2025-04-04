73b828096655dc463017e8279a9fd9defa69d759
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-04 17:53:54 +0200

    Edit search results form

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
+
 import { useSearchResult } from "../contexts/DefaultContext"
 
 export default function HomePage() {
-    const { searchText, setSearchText, searchResult, isLoaded } = useSearchResult();
-    console.log(searchResult);
+    const { searchText, setSearchText, searchResult, isLoaded } = useSearchResult()
+    const [searchTextValue, setSearchTextValue] = useState('')
 
     function handleSubmit(e) {
         e.preventDefault()
+
+        setSearchText(searchTextValue)
     }
 
     return (
@@ -15,7 +19,7 @@ export default function HomePage() {
             <form className="my-5" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="searchText" className="form-label">Search Movies</label>
-                    <input type="text" className="form-control" id="searchText" aria-describedby="searchText" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                    <input type="text" className="form-control" id="searchText" aria-describedby="searchText" value={searchTextValue} onChange={(e) => setSearchTextValue(e.target.value)} />
                 </div>
                 <button type="submit" className="btn btn-info">Submit</button>
             </form>
