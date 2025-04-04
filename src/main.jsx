43b4311b6c54b6769813a5efbcabc2322885a1a4
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-04 12:43:20 +0200

    Add project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import './css/resets.css'
+import './css/utilities.css'
+import './css/media.css'
 import './index.css'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
